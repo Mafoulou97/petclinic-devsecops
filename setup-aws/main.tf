aaa54765b2c893627682f954f79b368432866a83
Prosper <mafoulouprospectuce@gmail.com>
2026-07-25 18:41:51 +0000

    fix: alignement nom bucket S3 et version terraform

--- setup-aws/main.tf
+++ setup-aws/main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "petclinic-tf-state-prosper"
+    bucket         = "petclinic-tfstate-isi"  # Modifié ici !
     key            = "setup-aws/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "petclinic-tf-locks"
+    # dynamodb_table = "petclinic-tf-locks"   <-- Commente cette ligne si la table DynamoDB n'est pas encore créée
     encrypt        = true
   }
 }
@@ -16,7 +16,7 @@ provider "aws" {
 }
 
 data "aws_s3_bucket" "tf_state" {
-  bucket = "petclinic-tf-state-prosper"
+  bucket = "petclinic-tfstate-isi"  # Modifié ici aussi !
 }
 
 resource "aws_dynamodb_table" "tf_locks" {
